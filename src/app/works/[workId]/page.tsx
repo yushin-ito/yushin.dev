c4a5c9d2d1a3c508b2ae1c14e83bba524aea5679
yushin_ito <yitengyouxin8@gmail.com>
2025-03-09 19:22:29 +0900

    fix: buildが通るように修正

--- src/app/works/[workId]/page.tsx
+++ src/app/works/[workId]/page.tsx
@@ -1,39 +1,5 @@
-import { notFound, redirect } from "next/navigation";
+const WorkPage = () => {
+  return <div></div>;
+};
 
-async function getWorks(workId: Post["id"], userId: User["id"]) {
-  return await db.post.findFirst({
-    where: {
-      id: postId,
-      authorId: userId,
-    },
-  });
-}
-
-interface EditorPageProps {
-  params: { postId: string };
-}
-
-export default async function EditorPage({ params }: EditorPageProps) {
-  const user = await getCurrentUser();
-
-  if (!user) {
-    redirect(authOptions?.pages?.signIn || "/login");
-  }
-
-  const post = await getWorks(params.postId, user.id);
-
-  if (!post) {
-    notFound();
-  }
-
-  return (
-    <Editor
-      post={{
-        id: post.id,
-        title: post.title,
-        content: post.content,
-        published: post.published,
-      }}
-    />
-  );
-}
+export default WorkPage;
