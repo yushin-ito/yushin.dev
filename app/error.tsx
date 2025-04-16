8cc0adf9e61c4a0259db6784f5b4b5bbd25ff08f
Yushin Ito <yitengyouxin8@gmail.com>
2025-04-17 02:03:41 +0900

    fix: UIの軽微な修正

--- app/error.tsx
+++ app/error.tsx
@@ -11,8 +11,12 @@ const Error = ({ reset }: { reset: () => void }) => {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center space-y-16">
       <div className="space-y-4 text-center">
-        <h1 className="text-3xl font-bold">{t("metadata.title")}</h1>
-        <p className="text-sm text-muted-foreground">{t("metadata.description")}</p>
+        <h1 className="text-[min(5vw,36px)] font-bold">
+          {t("metadata.title")}
+        </h1>
+        <p className="text-xs text-muted-foreground sm:text-sm">
+          {t("metadata.description")}
+        </p>
       </div>
       <Button
         onClick={() => reset()}
